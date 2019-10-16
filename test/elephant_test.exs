67eac6ed6ebccee0c6b59962bb28b0d805897194
Steven Solomon <solomon.steven.m@gmail.com>
2019-10-16 04:49:50 -0400

    Add arity describe block

--- test/elephant_test.exs
+++ test/elephant_test.exs
@@ -11,68 +11,70 @@ defmodule ElephantTest do
     assert_process_stopped()
   end
 
-  test "returns truthy when zero arg function is called" do
-    {:ok, zero_arg_fn} = Elephant.mock(:zero_arg_name, 0)
+  describe "arity" do
+    test "returns truthy when zero arg function is called" do
+      {:ok, zero_arg_fn} = Elephant.mock(:zero_arg_name, 0)
 
-    zero_arg_fn.()
+      zero_arg_fn.()
 
-    assert Elephant.verify(:zero_arg_name, Elephant.once())
-  end
-
-  test "returns truthy when one arg function is called" do
-    {:ok, one_arg_fn} = Elephant.mock(:one_arg_name, 1)
-
-    one_arg_fn.("hello")
-
-    assert Elephant.verify(:one_arg_name, Elephant.once())
-  end
-
-  test "returns truthy when two arg function is called" do
-    {:ok, two_arg_fn} = Elephant.mock(:two_arg_name, 2)
-
-    two_arg_fn.("hello", "world")
-
-    assert Elephant.verify(:two_arg_name, Elephant.once())
-  end
-
-  test "returns truthy when three arg function is called" do
-    {:ok, three_arg_fn} = Elephant.mock(:three_arg_name, 3)
-
-    three_arg_fn.("hello", "world", "people")
-
-    assert Elephant.verify(:three_arg_name, Elephant.once())
-  end
-
-  test "returns truthy when four arg function is called" do
-    {:ok, four_arg_fn} = Elephant.mock(:four_arg_name, 4)
-
-    four_arg_fn.("hello", "world", "people", "hello")
-
-    assert Elephant.verify(:four_arg_name, Elephant.once())
-  end
-
-  test "returns truthy when five arg function is called" do
-    {:ok, five_arg_fn} = Elephant.mock(:five_arg_name, 5)
-
-    five_arg_fn.("hello", "world", "people", "hello", "world")
-
-    assert Elephant.verify(:five_arg_name, Elephant.once())
-  end
-
-  test "returns truthy when six arg function is called" do
-    {:ok, five_arg_fn} = Elephant.mock(:six_arg_name, 6)
-
-    five_arg_fn.("hello", "world", "people", "hello", "world", "people")
-
-    assert Elephant.verify(:six_arg_name, Elephant.once())
-  end
-
-  test "raises error when > 6 arg function passed to mock" do
-    assert_raise RuntimeError, "Arity greater than 6 is not supported.", fn ->
-      Elephant.mock(:name, 7)
+      assert Elephant.verify(:zero_arg_name, Elephant.once())
     end
 
-    assert_process_stopped()
+    test "returns truthy when one arg function is called" do
+      {:ok, one_arg_fn} = Elephant.mock(:one_arg_name, 1)
+
+      one_arg_fn.("hello")
+
+      assert Elephant.verify(:one_arg_name, Elephant.once())
+    end
+
+    test "returns truthy when two arg function is called" do
+      {:ok, two_arg_fn} = Elephant.mock(:two_arg_name, 2)
+
+      two_arg_fn.("hello", "world")
+
+      assert Elephant.verify(:two_arg_name, Elephant.once())
+    end
+
+    test "returns truthy when three arg function is called" do
+      {:ok, three_arg_fn} = Elephant.mock(:three_arg_name, 3)
+
+      three_arg_fn.("hello", "world", "people")
+
+      assert Elephant.verify(:three_arg_name, Elephant.once())
+    end
+
+    test "returns truthy when four arg function is called" do
+      {:ok, four_arg_fn} = Elephant.mock(:four_arg_name, 4)
+
+      four_arg_fn.("hello", "world", "people", "hello")
+
+      assert Elephant.verify(:four_arg_name, Elephant.once())
+    end
+
+    test "returns truthy when five arg function is called" do
+      {:ok, five_arg_fn} = Elephant.mock(:five_arg_name, 5)
+
+      five_arg_fn.("hello", "world", "people", "hello", "world")
+
+      assert Elephant.verify(:five_arg_name, Elephant.once())
+    end
+
+    test "returns truthy when six arg function is called" do
+      {:ok, five_arg_fn} = Elephant.mock(:six_arg_name, 6)
+
+      five_arg_fn.("hello", "world", "people", "hello", "world", "people")
+
+      assert Elephant.verify(:six_arg_name, Elephant.once())
+    end
+
+    test "raises error when > 6 arg function passed to mock" do
+      assert_raise RuntimeError, "Arity greater than 6 is not supported.", fn ->
+        Elephant.mock(:name, 7)
+      end
+
+      assert_process_stopped()
+    end
   end
 
   describe "process book keeping" do
