6e13d7620a7b8a79eae273a4ce9bc4b0e5e69b3c
Steven Solomon <solomon.steven.m@gmail.com>
2019-12-09 08:01:48 -0500

    Increment verstion to 0.2.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ExDoubles.MixProject do
       app: :exdoubles,
       description: description(),
       package: package(),
-      version: "0.1.2",
+      version: "0.2.0",
       elixir: "~> 1.8",
       deps: deps(),
       docs: [
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,5 +2,5 @@
 
 set -euxo -pipefail
 
-/Users/$(whoami)/.asdf/installs/elixir/1.8.2/.mix/escripts/ex_doc "ExDoubles" "0.1.2" _build/dev/lib/exdoubles/ebin -m "ExDoubles" -u https://github.com/steven-solomon/exdoubles
+/Users/$(whoami)/.asdf/installs/elixir/1.8.2/.mix/escripts/ex_doc "ExDoubles" "0.2.0" _build/dev/lib/exdoubles/ebin -m "ExDoubles" -u https://github.com/steven-solomon/exdoubles
 mix hex.publish
